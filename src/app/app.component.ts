9da28a9e5de1585118a33a564608854c7c7a01b9
alexantos <xandreale09@gmail.com>
2024-11-21 22:46:56 -0300

    Versão inicial drag and drop com card.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export interface Atributo {
+    descricao: string;
+    tipo: string;
+}
+
+export interface Tabela {
+    titulo: string;
+    atributos: Atributo[];
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'diagrama';
+    tabela: Tabela = {
+        titulo: 'Tabela de teste',
+        atributos: [
+            { descricao: 'atributo 1', tipo: 'varchar(128)' },
+            { descricao: 'atributo 2', tipo: 'int' },
+        ]
+    }
 }
